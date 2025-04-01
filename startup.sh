ce63fc4cf3500cf4e950b7584b8852b5108bb6b5
Lin Sun <lin.sun@solo.io>
2025-04-01 15:46:48 +0100

    preload image

--- startup.sh
+++ startup.sh
@@ -5,7 +5,8 @@ kind create cluster --config cluster.yaml
 
 docker pull docker.io/linsun/demo:v2
 docker pull docker.io/curlimages/curl
-docker pull docker.io/ollama/ollama:latest
+docker pull docker.io/linsun/rag:v1
+docker pull docker.io/linsun/rag:v2
 
 docker pull docker.io/istio/proxyv2:1.25.0-distroless
 docker pull docker.io/istio/pilot:1.25.0-distroless
@@ -13,9 +14,10 @@ docker pull docker.io/istio/install-cni:1.25.0-distroless
 docker pull docker.io/istio/ztunnel:1.25.0-distroless
 docker pull docker.io/istio/pilot:1.25.0-distroless
 
-
 kind load docker-image docker.io/linsun/demo:v2
 kind load docker-image docker.io/curlimages/curl
+kind load docker-image docker.io/linsun/rag:v1
+kind load docker-image docker.io/linsun/rag:v2
 
 kind load docker-image docker.io/istio/proxyv2:1.25.0-distroless
 kind load docker-image docker.io/istio/pilot:1.25.0-distroless
